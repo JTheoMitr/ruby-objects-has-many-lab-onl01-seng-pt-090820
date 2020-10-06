487660e2fd956f9bfef6d44a27daaed0455c645b
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2020-10-06 07:06:50 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -7,4 +7,21 @@ class Author
   end
   
   
+  def add_post(post)
+    post.author = self
+  end
+  
+  def add_post_by_name(name)
+    post = Post.new(name)
+    post.author = self
+  end
+  
+  def posts
+    Post.all.select { |post| post.author == self }
+  end
+  
+  def self.post_count
+    Post.all.size { |post| post.author == self }
+  end
+  
 end
